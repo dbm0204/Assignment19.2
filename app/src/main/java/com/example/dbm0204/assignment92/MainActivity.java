e9ca72d25167a90cbc47d66d74df5e059fb730a1
dbm0204 <dbm0204@gmail.com>
2017-07-10 19:05:25 +0530

    Need to implement JSON Object

--- app/src/main/java/com/example/dbm0204/assignment92/MainActivity.java
+++ app/src/main/java/com/example/dbm0204/assignment92/MainActivity.java
@@ -1,13 +1,80 @@
 package com.example.dbm0204.assignment92;
 
+import android.app.PendingIntent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Movie> listmodel= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=(RecyclerView)findViewById(R.id.recyclerView_Movie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.loadmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemid = item.getItemId();
+        switch (itemid) {
+            case R.id.load_menu:
+                new MyAsyncTask().execute("http://api.themoviedb.org/3/tv/top_rated?api_key=8496be0b2149805afa458ab8ec27560c");
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    class MyAsyncTask extends AsyncTask<String,Void,List<Movie>>
+    {
+        @Override
+        protected void onPreExecute(){
+            //checking status of connection
+            SupplicantState supState;
+            WifiManager wifiManager = (WifiManager) getSystemService(MainActivity.this.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            Toast.makeText(MainActivity.this, "Status:"+wifiInfo, Toast.LENGTH_SHORT).show();
+            supState = wifiInfo.getSupplicantState();
+            Toast.makeText(MainActivity.this, "Status:"+supState, Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager=(ConnectivityManager)getSystemService(MainActivity.this.CONNECTIVITY_SERVICE);
+            NetworkInfo activenetwork=connectivityManager.getActiveNetworkInfo();
+            if(activenetwork!=null && activenetwork.isConnected()==true )
+            {
+                Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_SHORT).show();
+            }else
+            {
+                Toast.makeText(MainActivity.this, "Connect to internet", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected List<Movie> doInBackground(String ... strings){
+            //TODO:Implement logic to parse the JSON Object
+
+        return null;
+
+    }
     }
 }
--- app/src/main/java/com/example/dbm0204/assignment92/MyAdapter.java
+++ app/src/main/java/com/example/dbm0204/assignment92/MyAdapter.java
@@ -6,47 +6,76 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import org.w3c.dom.Text;
 import java.util.List;
-
 /**
  * Created by dbm0204 on 7/10/17.
  */
-
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.viewHolder> {
 
-    Context context;
-    List<Movie> movieList;
-    public MyAdapter(Context context, List<Movie> movieList){
-        this.context =context;
-        movieList= movieList;
-    }
+    private Context context;
+    private final List<Movie> movieList;
+    private final OnItemClickListener listener;
 
+    /*
+    public interface  OnItemClicked{
+        void onItemClick(Movie position);
+    }
+    */
+    public MyAdapter(Context context, List<Movie> movieList, OnItemClickListener listener) {
+        this.context = context;
+        this.movieList = movieList;
+        this.listener = listener;
+    }
 
     @Override
     public MyAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.row,parent,false);
-        viewHolder viewHolder = new viewHolder(view,context);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+        viewHolder viewHolder = new viewHolder(view, context);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(MyAdapter.viewHolder holder, int position) {
-
-
+        Movie movie = movieList.get(position);
+        holder.movieName.setText(movie.getMovieName());
+        holder.NumVotes.setText(movie.getMovieVoteCount());
+        holder.movieId.setText(movie.getMovieId());
+        //holder.bind(movieList.get(position),listener);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movieList.size();
     }
-    class viewHolder extends RecyclerView.ViewHolder{
+
+    class viewHolder extends RecyclerView.ViewHolder {
         Context context;
+        TextView movieName;
+        TextView NumVotes;
+        TextView movieId;
 
 
-        public viewHolder(View itemView,Context context) {
+        viewHolder(View itemView, Context context) {
             super(itemView);
-            this.context=context;
+            this.context = context;
+            movieId = (TextView) itemView.findViewById(R.id.movieID);
+            NumVotes = (TextView) itemView.findViewById(R.id.textView_vote);
+            movieName = (TextView) itemView.findViewById(R.id.textView_movieName);
+
         }
+        /**
+         *
+         public void bind(final Movie item, final OnItemClickListener listener){
+         movieName.setText(item.getMovieName());
+         itemView.setOnClickListener(new View.OnClickListener(){
+        @Override public void onClick(View view){
+        listener.onItemClick(item);
+        }
+        });
+         }*/
     }
 }
